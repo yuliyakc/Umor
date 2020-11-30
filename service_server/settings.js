10ec38eeb441da644aaab2b12221732ac83e3b67
yuliyakc <yuliyakc21@gmail.com>
2020-11-30 23:49:05 +0200

    Code

--- service_server/settings.js
+++ service_server/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: 3000,
     TOKEN_PATH: './service_server/secret/token.json',
-    CREDENTIALS_PATH: './service_server/secret/credentials.json',
-    EMAIL_FROM: "Yuliya Kozina"
+    CREDENTIALS_PATH: './service_server/secret/client_secret.json',
+    EMAIL_FROM: "antonina.krasnoshchok@keenethics.com"
 };
